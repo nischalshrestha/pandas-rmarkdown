e8bfb824ab925331f5051e96f3b8912275d84c8f
nischalshrestha <bitsorific@gmail.com>
2020-11-14 20:23:50 -0500

    save progress on article

--- R/utils.R
+++ R/utils.R
@@ -37,6 +37,8 @@ python_df <- function(pydf) {
   } else {
     rdf <- reticulate::py_to_r(pydf)
   }
+  # TODO format floats to use 6 decimal places
+  
   # 2) Turn some data types back to Python representation
   rdf <- rdf %>%
     purrr::map_df(~ ifelse(is.na(.), "NaN", as.character(.))) %>%
@@ -44,117 +46,5 @@ python_df <- function(pydf) {
   # 3) change to 0-indexing for row index
   rownames(rdf) <- as.numeric(rownames(rdf)) - 1
   rdf
-}
-
-# TODO test this 
-group_columns <- function(columns, start = 2) {
-  colDefs <- list()
-  start <- start + 1
-  range <- start:length(columns)
-  # make first column be as is
-  colDefs[[columns[1]]] <- reactable::colDef(name = columns[1])
-  # make
-  for (i in range) {
-    colDefs[[columns[i]]] <- reactable::colDef(
-      name = " "
-    )
-  }
-  colGroups <- list()
-  colGroups <-
-    lapply(
-      columns[start:length(columns)],
-      function(column) {
-        reactable::colGroup(name = column, columns = c(column))
-      }
-    )
-  list(colDefs, colGroups)
-}
-
-# TODO this is now testable so test this with multiple examples
-# the options is just used for knitr context and could probably be NULL by default
-format_python_df <- function(options = NULL, raw_result, do_knit_print = FALSE) {
-  is_multi_index <- "pandas.core.indexes.multi.MultiIndex" %in% class(raw_result$index)
-  
-  # wizard of oz pandas dataframe by changing index as well
-  converted_result <- python_df(raw_result)
-  # debug_print(options$debug, "prepped a dataframe")
-  
-  # handle reactable for df with MultiIndex
-  if (isTRUE(is_multi_index)) {
-    cat('multiindex\n')
-    # handle the case where we might already have renamed axis for Index
-    # in this case, we should not show index, and group one more
-    show_index <- !identical(rdf[[1]], rownames(rdf))
-    grp_cols <- group_columns(
-      colnames(converted_result),
-      start = ifelse(show_index, 1, 2)
-    )
-    table <- reactable::reactable(
-      converted_result,
-      compact = TRUE,
-      rownames = show_index,
-      columns = grp_cols[[1]],
-      columnGroups = grp_cols[[2]]
-    )
-    if (do_knit_print) {
-      return(htmltools::knit_print.shiny.tag.list(table))
-    }
-    return(table)
-  } else {
-    table <- reactable::reactable(
-      converted_result,
-      pagination = TRUE,
-      minRows = 10,
-      compact = TRUE,
-      # height = 300,
-      highlight = TRUE,
-      bordered = TRUE,
-      rownames = TRUE,
-      resizable = TRUE,
-      theme = reactable::reactableTheme(
-        borderWidth = "2px"
-      ),
-      defaultColDef = reactable::colDef(
-        cell = function(value, index) {
-          # turn NULL into NaN
-          if (is.na(value) || is.null(value) || value == "") {
-            return(as.character("NaN"))
-          }
-          return(value)
-        }
-      ),
-      columns = list(
-        .rownames = reactable::colDef(
-          style = list(
-            textAlign = "left"
-          )
-        )
-      )
-    )
-    if (do_knit_print) {
-      return(htmltools::knit_print.shiny.tag.list(table))
-    }
-    return(table)
-  }
-}
-
-#' Takes a number of lists and appends them into a single list.
-#' Instead of doing nested append calls, this will automate that
-#' by doing the series of append calls for you.
-#'
-#' @param ...
-#'
-#' @return named list
-#'
-#' @examples
-reappend <- function(...) {
-  myList <- list()
-  lists <- list(...)
-  range <- 1:length(lists)
-  # # Now the new experiments
-  for (i in seq_along(range)) {
-    myList <- append(myList, lists[[i]])
-  }
-  myList
 }
 
--- kable_experiment.R
+++ kable_experiment.R
@@ -6,19 +6,14 @@ library(reticulate)
 
 source(here::here("R/utils.R"))
 
-kable_pandas <- function(pydf_name = "df") {
+kable_pandas <- function(pydf, rmd = FALSE) {
   # grab the python df
   # note how we use py_eval instead of using py$df since we want to retain the python
   # structure still to check things like MultiIndex
-  pydf <- reticulate::py_eval(pydf_name)
-  # regular df case
-  # pydf <- reticulate::py_eval("pd.DataFrame(np.random.randn(8, 4), columns=['a','b','c','d'])")
+  # pydf <- reticulate::py_eval(pydf_name)
   
-  # WoZ begins
   # setup an R dataframe based on `pydf`
   rdf <- python_df(pydf)
-  # regular df case
-  # rdf <- py$df 
   
   # grab the regular columns not part of the Index
   columns <- pydf$columns$values
@@ -26,24 +21,33 @@ kable_pandas <- function(pydf_name = "df") {
   rdf_column_names <- colnames(rdf)
   row_index_cols <- rdf_column_names[!(rdf_column_names %in% columns)]
   
-  # this is the amount of space for the Index
+  # this is the amount of space for the Index columns for grouping
   idx_column_space <- sum(!(colnames(rdf) %in% columns))
-  # this is the space for the rest of columns
+  # this is the space for the rest of columns for grouping
   column_space <- rep(1, length(columns))
   names(column_space) <- columns
   
   # base kbl
-  setup_kbl <- rbind(rdf, rdf) %>% 
+  setup_kbl <- rdf %>% 
     # this retains color for the row Index columns
     rename_with(function(x) cell_spec(x, "html", color = "black"), any_of(row_index_cols)) %>% 
-    kableExtra::kbl(align = "l", escape = F, row.names = T) %>%
-    kableExtra::kable_styling(bootstrap_options = c("hover"), full_width = F, position = "left")
+    kableExtra::kbl(align = "l", escape = F, row.names = T) 
+  
+  if (rmd) {
+    setup_kbl <- setup_kbl %>%
+      kableExtra::kable_paper()
+  } else {
+    setup_kbl <- setup_kbl %>%
+      kableExtra::kable_styling(bootstrap_options = c("hover"), full_width = F, position = "center")
+  }
   
   final_kbl <- setup_kbl
-  # check if it is a MultiIndex
+  # check if it is a MultiIndex, `reticulate` will list other classes but the very first
+  # one is the Python class
   if (class(pydf$index)[[1]] == "pandas.core.indexes.multi.MultiIndex") {
     # TODO a check to see if we have many more levels in which case we have to stack
-    # the column levels and sometimes above the row indices
+    # the column levels and sometimes above the row indices. In such a case, we do 
+    # not "raise" the columns and can skip coloring rows white
     
     # if so, we "raise" the regular columns by making the columns invisible 
     # and moving them to a header above
@@ -55,32 +59,31 @@ kable_pandas <- function(pydf_name = "df") {
   
   final_kbl
 }
-
-
-# this right assign may look weird, but actually it's great in cases when  
-# indentation matters, such as python code string since we can properly
-# copy paste almost as is
-"
-import pandas as pd
-import numpy as np
-
-# create pd.MultiIndex
-arrays = [np.array(['bar', 'bar', 'baz', 'baz', 'foo', 'foo', 'qux', 'qux']),
-         np.array(['one', 'two', 'one', 'two', 'one', 'two', 'one', 'two'])]
-tuples = list(zip(*arrays))
-index = pd.MultiIndex.from_tuples(tuples, names=['first', 'second'])
-
-# make a df with its Index as the MultiIndex
-df = pd.DataFrame(np.random.randn(8, 4), columns=['a','b','c','d'])
-df_multi = pd.DataFrame(np.random.randn(8, 4), index = index, columns=['a','b','c','d'])
-
-" -> setup_code
-
-# run setup code
-reticulate::py_run_string(setup_code)
-
-# test cases
-kable_pandas("df")
+# 
+# # this right assign may look weird, but actually it's great in cases when  
+# # indentation matters, such as python code string since we can properly
+# # copy paste almost as is
+# "
+# import pandas as pd
+# import numpy as np
+# 
+# # create pd.MultiIndex
+# arrays = [np.array(['bar', 'bar', 'baz', 'baz', 'foo', 'foo', 'qux', 'qux']),
+#          np.array(['one', 'two', 'one', 'two', 'one', 'two', 'one', 'two'])]
+# tuples = list(zip(*arrays))
+# index = pd.MultiIndex.from_tuples(tuples, names=['first', 'second'])
+# 
+# # make a df with its Index as the MultiIndex
+# df = pd.DataFrame(np.random.randn(8, 4), columns=['a','b','c','d'])
+# df_multi = pd.DataFrame(np.random.randn(8, 4), index = index, columns=['a','b','c','d'])
+# 
+# " -> setup_code
+# 
+# # run setup code
+# reticulate::py_run_string(setup_code)
+# 
+# # test cases
+# kable_pandas("df")
 # kable_pandas("df_multi")
 
 
